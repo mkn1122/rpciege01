f55f26094ff8cffd3bc376175a9913d8cdfdb4b8
mkn1122 <lapak4dbuka@gmail.com>
2023-06-10 07:19:19 +0000

    1

--- game2/src/lib.rs
+++ game2/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#![no_std]
+use soroban_sdk::{contractimpl, Address, Env, String};
+pub struct Contract;
+#[contractimpl]
+impl Contract {
+    pub fn game_2(env: Env, _nft_dest: Address) -> String {
+        let s = String::from_slice(&env, "1694-1727");
+        s
     }
 }
